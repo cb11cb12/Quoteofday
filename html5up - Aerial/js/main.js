610138090798e4cff37399ceed6eaa1fde7040b5
cb11cb12 <cbbrandi11@gmail.com>
2023-06-17 09:13:00 -0500

    updated api

--- html5up - Aerial/js/main.js
+++ html5up - Aerial/js/main.js
@@ -1,18 +1,18 @@
-//Example fetch using favquotes.com
+//Example fetch using api.quotable.io
 
 
 document.querySelector('button').addEventListener('click', quoteOfDay)
 
 function quoteOfDay(){
   // let search = document.querySelector('input').value 
-  const url = 'https://favqs.com/api/qotd'
+  const url = 'https://api.quotable.io/quotes/random'
 
   fetch(url)
       .then(res => res.json())// parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('h3').innerText = `by ${data.quote.author}`
-        document.querySelector('h2').innerText = `"${data.quote.body}"`
+        document.querySelector('h3').innerText = `by ${data[0].author}`
+        document.querySelector('h2').innerText = `"${data[0].content}"`
         
         // document.querySelector('img').src = data[0].url
       })
@@ -23,6 +23,6 @@ function quoteOfDay(){
   
     }
     
-    // api_key=17c97fb2-af11-4d9a-b149-fb1dc7e0e904
+    
 
    

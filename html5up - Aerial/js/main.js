043ba63be737c144c780a2a4ed418a97b54e6f37
cb11cb12 <cbbrandi11@gmail.com>
2023-06-17 09:21:32 -0500

    Changed to jquery

--- html5up - Aerial/js/main.js
+++ html5up - Aerial/js/main.js
@@ -1,27 +1,46 @@
-//Example fetch using api.quotable.io
-
-
-document.querySelector('button').addEventListener('click', quoteOfDay)
+$('button').on('click', quoteOfDay);
 
 function quoteOfDay(){
-  // let search = document.querySelector('input').value 
-  const url = 'https://api.quotable.io/quotes/random'
-
-  fetch(url)
-      .then(res => res.json())// parse response as JSON
-      .then(data => {
-        console.log(data)
-        document.querySelector('h3').innerText = `by ${data[0].author}`
-        document.querySelector('h2').innerText = `"${data[0].content}"`
-        
-        // document.querySelector('img').src = data[0].url
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-
-  
+  $.ajax({
+    url: 'https://api.quotable.io/quotes/random',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      $('h3').text(`by ${data[0].author}`);
+      $('h2').text(`"${data[0].content}"`);
+      // $('img').attr('src', data.url);
+    },
+    error: function(err) {
+      console.log(`error ${err}`);
     }
+  });
+}
+
+
+
+
+// //Example fetch using api.quotable.io
+
+
+// document.querySelector('button').addEventListener('click', quoteOfDay)
+
+// function quoteOfDay(){
+//   // let search = document.querySelector('input').value 
+//   const url = 'https://api.quotable.io/quotes/random'
+
+//   fetch(url)
+//       .then(res => res.json())// parse response as JSON
+//       .then(data => {
+//         console.log(data)
+//         document.querySelector('h3').innerText = `by ${data[0].author}`
+//         document.querySelector('h2').innerText = `"${data[0].content}"`
+        
+//         // document.querySelector('img').src = data[0].url
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+//     }
     
     
 
